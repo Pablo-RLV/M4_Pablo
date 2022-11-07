85da61018981bc8a400e2e2757e7212ec5138102
Pablo-RLV <pablo.viana@sou.inteli.edu.br>
2022-11-07 01:43:37 -0300

    update 1

--- SEMANA_03/EXERCICIOS/primeiro_projeto/primeiro_projeto.ino
+++ SEMANA_03/EXERCICIOS/primeiro_projeto/primeiro_projeto.ino
@@ -3,10 +3,12 @@ int led2 = 5;
 int led3 = 6;
 int led4 = 7;
 int porta_sensor = 8;
+int porta_buzzer = 3;
 int vetor[4] = {led1, led2, led3, led4};
 int valor_sensor = 0;
 int binario[4]={0,0,0,0};
 int vetor_binario[4] = {0, 0, 0, 0};
+int i = 0;
 void setup() {
   Serial.begin(115200);
   for(int portas=0; portas <= 3; portas++){
@@ -17,8 +19,9 @@ void setup() {
 void loop() {
   valor_sensor = analogRead(porta_sensor);
   int valor_convertido = valor_sensor / 537;
+  tone(porta_buzzer, 50 * valor_convertido);
 
-  int i = 0;
+  i = 0;
   while (valor_convertido > 0 && i < 4) {
     binario[i] = valor_convertido % 2;
     valor_convertido = valor_convertido / 2;
@@ -32,6 +35,8 @@ void loop() {
   }
   delay(100);
 }
+
+
 
 
 
